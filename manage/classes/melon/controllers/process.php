fdbdd9f844693337c3928ea3c350eea3f75a1a7a
Developer <cfm@Developers-iMac.local>
2018-04-19 09:40:46 -0400

    auto push all to test server

--- manage/classes/melon/controllers/process.php
+++ manage/classes/melon/controllers/process.php
@@ -20,68 +20,36 @@
 
 		function index(){
 
+		
+			$M = new \Melon\Models\WordToTei();
 
+			
 			//---- load text
 			$text = file_get_contents(\MHS\Env::APP_INSTALL_DIR . "/incl/". $this::EXTRAS_FILE);
 			
-
+			$M->text($text);
+			
+			$M->prepOxFile();
+			
+			$M->separateDocParts();
 	
-			//---- prep OX output
 	
-			// add line breaks
-			
-			$endTags = [ "Desc>", "Stmt>", "Info>", "Change>", "text>", "body>", "</p>"];
-			$endTagsModded = [ "Desc>\n", "Stmt>\n", "Info>\n", "Change>\n", "text>\n", "body>\n", "</p>\n"];
-			
-			$text = str_replace($endTags, $endTagsModded, $text);
-
-			
-			
-			//remove unnecessary strings
-			$remove = [' rend="Normal"'];
-			$text = str_replace($remove, "", $text);
-			
-			
-			file_put_contents(\MHS\Env::APP_INSTALL_DIR . "/incl/". $this::EXTRAS_FILE_BR, $text);
-
-
-
-
-
-			
-			$this->LBL = new \MHS\TxtProcessor\LineByLine();
-			$LBL = $this->LBL;
-			
-			$LBL->setText($chunk);
-			
-			
-			
-			//---- global replacements
-/*			
-			$LBL->find("name")->replaceWith("friend");
-			$LBL->find("joe")->replaceWith("<b>jill</b>");
-			$LBL->find("##ENDITAL##")->replaceWith("</i>");
-*/
-
-			//---- more detailed line-by-line processing
-
-			$LBL->forEachLine(function($line){
+			//if we have chunks, that is, more than one document
+			if(strpos($text, '{{DOC}}')) {
 				
-				if($line->beginsWith("Name:")) $this->template->gleanName($line);
-				//if($line->beginsWith("Birth:")) $this->template->gleanBirth($line);
-
-				if($line->beginsWith("<p>{{ADDRESS}}")) $this->LBL->newSection("<div type=\"addr\">", "</div>");
-
-				
-				//add line to output
-//				$this->LBL->appendLine($line);
-
-			});
-
+			}
 			
-			print $this->template->getOutput();
-			
-//			print $this->LBL->getOutput();
+			//single doc
+			else {
+				$M->processBody();
+			}
+
+
+
+			file_put_contents(\MHS\Env::APP_INSTALL_DIR . "/incl/". $this::EXTRAS_FILE_BR, $M->text());
 		}
+	
+		
+
 
 	} //class
---
+++ manage/classes/melon/models/wordtotei.php
@@ -0,0 +1,156 @@
+<?php
+
+
+	namespace Melon\Models;
+	
+	
+	
+	class WordToTei extends \MHS\TxtProcessor\LineByLine {
+	
+		private $teiHeader = "";
+		
+		private $teiDocMain = "";
+		
+		private $teiDocEnding = "";
+	
+		//array of individual docs with the XML
+		private $chunks = [];
+
+
+
+		public function text($text = false){
+			if(false == $text) return $this->text;
+			else $this->text = $text;
+		}
+	
+	
+	
+
+
+		public function prepOxFile(){
+					//---- prep OX output
+
+			// add line breaks
+			$endTags = [ "Desc>", "Stmt>", "Info>", "Change>", "text>", "body>", "</p>"];
+			$endTagsModded = [ "Desc>\n", "Stmt>\n", "Info>\n", "Change>\n", "text>\n", "body>\n", "</p>\n"];
+			
+			$this->text = str_replace($endTags, $endTagsModded, $this->text);
+
+			//remove unnecessary strings
+			$remove = [' rend="Normal"'];
+			$this->text = str_replace($remove, "", $this->text);
+		}
+		
+		
+		
+		
+		public function separateDocParts(){
+			$parts1 = explode("<body>\n", $this->text);
+			
+			$parts2 = explode("</body>\n", $parts1[1]);
+			
+			$this->teiHeader = $parts1[0] . "<body>\n";
+			
+			$this->teiDocMain =  $parts2[0];
+			
+			$this->teiDocEnding = "</body>\n" . $parts2[1];
+		}
+
+
+
+
+
+		public function chunkByChunk(){
+			
+			$this->chunks = explode("<p>{{DOC}}</p>\n", $this->teiDocMain);
+			
+die("WordToTei->chunkByChunk() not yet implemented");			
+		}
+		
+		
+		
+		/* this also accepts a string input (coming from chunkByChunk), but
+		 * when $text === false, it uses full teiDocMain as text
+		 */
+		public	function processBody($text = false){
+				
+			if($text === false) $text = $this->teiDocMain;
+			
+			$this->setText($text);
+
+	
+			//first glean some metadata we need
+			
+			
+			//add document beginning
+			$this->appendText("<div type=\"doc\">\n");
+			
+			//---- more detailed line-by-line processing
+			$this->forEachLine(function($line){
+				
+				//rest of the body lines and sections
+/*				if($line->contains("{{DATELINE}}")) $this->LBL->newSection("<dateline>", "</dateline>");
+				else if($line->contains("{{SALUTE}}")) {
+					$this->LBL->newSection("<salute>", "</salute>");
+					$this->LBL->appendOutput($line);
+					$this->LBL->closeSection();
+					return;
+				}
+*/
+
+				if($line->contains("{{SIGNED}}")) $this->newSection("<closer>", "</closer>");
+				else if($line->contains("{{PS}}")) $this->newSection("<postscript>", "</postscript>");
+				else if($line->contains("{{ADDRESS}}")) $this->newSection("<div type=\"addr\">", "</div>");
+				else if($line->contains("{{SOURCE}}")) $this->newSection("<div type=\"source\">", "</div>");
+				else if($line->contains("{{NOTE}}")) $this->newSection("<div type=\"note\">", "</div>");
+				else if($line->contains("{{INSERTION}}")) $this->newSection("<div type=\"insertion\">", "</div>");
+				
+				//add line to output
+				$this->appendOutput($line);
+
+			});
+			//close any sections
+			$this->closeSection();
+			
+			//close document
+			$this->appendText("</div><!-- //document -->\n");
+
+			print $this->getOutput();
+		}
+	
+			
+		
+
+
+
+		private function parseDate($line){
+			preg_match("/^<p>(.+)<\/p>$/", $line->text, $matches);
+			
+			if(!isset($matches[1])) return;
+			
+			//separate
+			$parts = explode("-", $matches[1]);
+
+			//day
+			if(!isset($parts[2])) $day = "00";
+			else {
+				$day = $parts[2];
+			}
+			
+			//month
+			if(!isset($parts[1])) $month = "00";
+			else {
+				if(!is_numeric($parts[1][0])) {
+					
+				}
+			}
+			
+			$this->LBL->appendText("\n");
+		
+			$this->lineCount++;
+			return;
+		}
+	
+	
+	
+	}
