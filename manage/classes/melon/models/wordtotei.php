9be4645958a6d37a8a3def3f56f7fde5f06121b8
bb <bbeck@masshist.org>
2018-05-04 14:40:53 -0400

    auto push all to test server

--- manage/classes/melon/models/wordtotei.php
+++ manage/classes/melon/models/wordtotei.php
@@ -2,17 +2,17 @@
 
 
 	namespace Melon\Models;
-	
-	
-	
+
+
+
 	class WordToTei extends \MHS\TxtProcessor\LineByLine {
-	
+
 		private $teiHeader = "";
-		
+
 		private $teiDocMain = "";
-		
+
 		private $teiDocEnding = "";
-	
+
 		//array of individual docs with the XML
 		private $chunks = [];
 
@@ -22,9 +22,9 @@
 			if(false == $text) return $this->text;
 			else $this->text = $text;
 		}
-	
-	
-	
+
+
+
 
 
 		public function prepOxFile(){
@@ -33,26 +33,26 @@
 			// add line breaks
 			$endTags = [ "Desc>", "Stmt>", "Info>", "Change>", "text>", "body>", "</p>"];
 			$endTagsModded = [ "Desc>\n", "Stmt>\n", "Info>\n", "Change>\n", "text>\n", "body>\n", "</p>\n"];
-			
+
 			$this->text = str_replace($endTags, $endTagsModded, $this->text);
 
 			//remove unnecessary strings
 			$remove = [' rend="Normal"'];
 			$this->text = str_replace($remove, "", $this->text);
 		}
-		
-		
-		
-		
+
+
+
+
 		public function separateDocParts(){
 			$parts1 = explode("<body>\n", $this->text);
-			
+
 			$parts2 = explode("</body>\n", $parts1[1]);
-			
+
 			$this->teiHeader = $parts1[0] . "<body>\n";
-			
+
 			$this->teiDocMain =  $parts2[0];
-			
+
 			$this->teiDocEnding = "</body>\n" . $parts2[1];
 		}
 
@@ -61,36 +61,37 @@
 
 
 		public function chunkByChunk(){
-			
+
 			$this->chunks = explode("<p>{{DOC}}</p>\n", $this->teiDocMain);
-			
-die("WordToTei->chunkByChunk() not yet implemented");			
+
+die("WordToTei->chunkByChunk() not yet implemented");
 		}
-		
-		
+
+
 
 		public function gleanMetadata(){
-			
-			$this->parseDate($this->deleteParagraphContaining("{{DATELINE}}"));
-			
+
+			$this->parseDate($this->findString("{{DATELINE}}")->inParagraph()->remove()->getMatches());
+			$this->parseDate($this->findString("{{DATELINE}}")->inParagraph()->unwrap()->getMatches());
+
 		}
-		
+
 
 
 		/* this also accepts a string input (coming from chunkByChunk), but
 		 * when $text === false, it uses full teiDocMain as text
 		 */
 		public	function processDocument($text = false){
-				
+
 			if($text === false) $text = $this->teiDocMain;
-			
+
 			$this->setText($text);
 
 			$this->gleanMetadata();
 
-			//add document beginning
+			//add document beginning with placeholders to pop in metadata once we find it
 			$this->appendText("<div type=\"doc\">{{HEAD}}\n{{BIBL}}\n<div type=\"docbody\">\n{{OPENER}}");
-			
+
 			//---- more detailed line-by-line processing
 			$this->forEachLine(function($line){
 
@@ -100,34 +101,36 @@ die("WordToTei->chunkByChunk() not yet implemented");
 				else if($line->contains("{{SOURCE}}")) $this->newSection("<div type=\"source\">", "</div>");
 				else if($line->contains("{{NOTE}}")) $this->newSection("<div type=\"note\">", "</div>");
 				else if($line->contains("{{INSERTION}}")) $this->newSection("<div type=\"insertion\">", "</div>");
-				
+
 				//add line to output
+				$line->trimLeading(); //remove the matching pattern
+
 				$this->appendOutput($line);
 
 			});
 			//close any sections
 			$this->closeSection();
-			
+
 			//close document
 			$this->appendText("</div>\n</div><!-- //document -->\n");
 
 			//swap in pre-gathered metadata
-			
-			
-			
+
+
+
 			print $this->getOutput();
 		}
-	
-			
-		
+
+
+
 
 
 
 		private function parseDate($matchArray){
 			preg_match("/^<p>(.+)<\/p>$/", $line->text, $matches);
-			
+
 			if(!isset($matches[1])) return;
-			
+
 			//separate
 			$parts = explode("-", $matches[1]);
 
@@ -136,21 +139,21 @@ die("WordToTei->chunkByChunk() not yet implemented");
 			else {
 				$day = $parts[2];
 			}
-			
+
 			//month
 			if(!isset($parts[1])) $month = "00";
 			else {
 				if(!is_numeric($parts[1][0])) {
-					
+
 				}
 			}
-			
+
 			$this->LBL->appendText("\n");
-		
+
 			$this->lineCount++;
 			return;
 		}
-	
-	
-	
+
+
+
 	}
